aa45c99bbfd6128e4856904cac6fddba07995a06
Robby <113494281+ranjotdharni@users.noreply.github.com>
2023-03-30 12:56:53 -0700

    Update settings.py

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['meteorize-backend.onrender.com']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
