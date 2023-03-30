ef867de9c6924e9f2905cd47090a1e607933fac9
Robby <113494281+ranjotdharni@users.noreply.github.com>
2023-03-30 13:17:55 -0700

    Update views.py

--- backend/server/views.py
+++ backend/server/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 # Create your views here.
 def index(request):
-    return JsonResponse({"status":200, "message":"also conn. success..."})
+    return JsonResponse({"status":200, "message":"Django Unchained"})
 
 def home(request):
-    return JsonResponse({"status":200, "message":"server connected..."})
+    return JsonResponse({"status":200, "message":"Pulp Fiction"})
