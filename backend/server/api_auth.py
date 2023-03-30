34395dec76a7acb609396df988ea737570bed8a6
Robby <113494281+ranjotdharni@users.noreply.github.com>
2023-03-30 14:27:21 -0700

    adding api auth

---
+++ backend/server/api_auth.py
@@ -0,0 +1,11 @@
+class CustomMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        print("API Auth. middleware fired!")
+
+        response = self.get_response(request)
+
+        return response
