d222341c712309d46f8659ff28bd1bdc9ecb22b4
Robby <113494281+ranjotdharni@users.noreply.github.com>
2023-03-30 14:50:37 -0700

    unblocking

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -39,7 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'api_auth.middleware.CustomMiddleware',
+    'api_auth.CustomMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
