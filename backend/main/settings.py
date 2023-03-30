d23380268d85e2e6ed2b99a95f46f8422540230f
Robby <113494281+ranjotdharni@users.noreply.github.com>
2023-03-29 23:42:00 -0700

    Update settings.py

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u+lw013@!cg^&4!j+qf7wk))xtxfrm0ipnnzq9rwtchdl1bw(d'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
