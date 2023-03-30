2e4bcefa00442ed0b95c7c0e1d132730462b5326
Robby <113494281+ranjotdharni@users.noreply.github.com>
2023-03-30 14:40:13 -0700

    Update settings.py

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -39,7 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'api_auth.CustomMiddleware',
+    'api_auth.middleware.CustomMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
