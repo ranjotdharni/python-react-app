ef2c4c4c30e59fca76717c1b34e235268c85e76f
Robby <113494281+ranjotdharni@users.noreply.github.com>
2023-04-12 16:54:59 -0700

    Update page.jsx

--- frontend/csr/app/page.jsx
+++ frontend/csr/app/page.jsx
@@ -1,10 +1,22 @@
 
-export default function HomePage()
+export default function HomePage({data})
 {
     return (
         <div>
             <h1>Welcome to the home page!</h1>
-            <p>Some Content</p>
+            <p>data.message</p>
         </div>
     );
+}
+
+export async function getServerSideProps()
+{
+    const res = await fetch('https://meteorize-backend.onrender.com/v1', {
+      method: "GET",
+      headers: {
+        "AUTH-TOKEN": process.env.AUTH_TOKEN,
+      }
+    });
+    const data = await res.json();
+    return {props: {data}}
 }
