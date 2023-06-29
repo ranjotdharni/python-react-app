fb92fd2036a25448846ebe765d7ef92f93414028
Robby <113494281+ranjotdharni@users.noreply.github.com>
2023-06-29 14:03:07 -0700

    background rework

--- frontend/csr/app/(components)/DynamicWall.js
+++ frontend/csr/app/(components)/DynamicWall.js
@@ -1,11 +1,12 @@
+import WallCover from './WallCover';
 import styles from './component.module.css';
 
 export default async function DynamicWall() {
-  const background = await defaultBackground();
+  const background = await defaultBackground().then((bg) => { return {'img' : bg, 'isReady' : true} });
 
   return (
-    <div className={styles.background} style={{background: 'url(' + background + ')'}}>
-
+    <div className={styles.background} style={{background: 'url(' + background.img + ')'}}>
+        <WallCover isReady={background.isReady}/>
     </div>
   );
 }
