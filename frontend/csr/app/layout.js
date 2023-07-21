f45e1d6d38eea91c43c30afc118a59c41c7043c6
Robby <113494281+ranjotdharni@users.noreply.github.com>
2023-07-20 21:40:16 -0700

    Update layout.js

--- frontend/csr/app/layout.js
+++ frontend/csr/app/layout.js
@@ -13,6 +13,9 @@ const page_font = Dosis({
 export default async function RootLayout({ children }) {
   return (
     <html className={page_font}>
+      <head>
+      <meta name="google-site-verification" content="BUVhXG9V9Xkh9xoR1Dvwyv1I8QDT2tuNDk2bTWh-2YM" />
+      </head>
       <body>
         <NavBar />
         <DynamicWall />
